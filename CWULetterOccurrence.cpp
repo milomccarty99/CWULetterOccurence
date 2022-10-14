4e9c7dedb687965d3e6db1b0d10a4f0946c36e0e
milomccarty99 <milomccarty99@github.com>
2022-10-14 03:28:21 -0700

    fixed memory allocation problems

--- CWULetterOccurrence.cpp
+++ CWULetterOccurrence.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <fstream>
 #include <string>
+#include <tuple> // for sorting functionality
 #include <sys/wait.h> // wait functionality
 #include <sys/mman.h> // mmap functionality
 #include <stdlib.h>
@@ -12,7 +13,8 @@
 #define NUMINTS (256) // we will examine all ascii chars
 #define LOWERCASE_OFFSET (97)
 #define UPPERCASE_OFFSET (65)
-#define FILESIZE (NUMINTS *sizeof(int))
+#define ALPHABET_LEN (26)
+#define MMAP_FILESIZE (NUMINTS * sizeof(int))
 using namespace std;
 
 
@@ -20,9 +22,9 @@ class OccurrenceHandler {
 	public:
 	//iterates through the file provided (filename) to find number of char c in file
 		int singleLetter(string filename, char c) {
+			fstream ReadFile(filename);
 			string line;
 			int count = 0;
-			ifstream ReadFile(filename);
 			while(getline(ReadFile, line))
 			{
 				for (int i = 0; i < line.length(); i++)
@@ -37,8 +39,54 @@ class OccurrenceHandler {
 			return count;
 		}
 
-};
+		
 
+};
+//helper method for quickSort
+int partition(tuple<char,int>* data, int low, int high)
+{
+	tuple<char, int> pivot = data[high];
+
+	int i = low-1;
+
+	for(int j = low; j< high; j++)
+	{
+		if(get<1>(data[j]) > get<1>(pivot)) // sort high to low
+		{
+			i++;
+			tuple<char, int> temp_swap = data[i];
+			data[i] = data[j];
+			data[j] = temp_swap;
+		}
+	}
+	tuple<char,int> temp = data[i+1];
+	data[i+1] = data[high];
+	data[high]= temp;
+	
+	return i+1;
+
+}
+// quickSort method for sorting characters
+tuple<char, int>* quickSort(tuple<char,int>* data,int low, int high)
+{
+	if(low < high)
+	{
+		int pi = partition(data, low, high);
+		quickSort(data, low, pi -1);
+		quickSort(data, pi+1, high);
+	}
+	return data;
+}
+
+void outputData(tuple<char, int> * data, int size)
+{
+	for(int i = 0; i < size; i++)
+	{
+		cout << get<0>(data[i]) << ": " << get<1>(data[i]) << endl;
+	}
+}
+
+//
 int main(int argc, char** argv)
 {
 	// arguments
@@ -47,7 +95,7 @@ int main(int argc, char** argv)
 	// -p letter pairs (seperate process)
 	// -v visual
 
-	string filename = "blah.txt";
+	string filename = "file not provided";
 	for(int i = 1; i < argc; i++)
 	{
 		if(argv[i][0] != '-')
@@ -60,16 +108,9 @@ int main(int argc, char** argv)
 		}
 	}
 
-	cout << filename << endl;
-	int c = 5;
-	int *cptr = &c;
-
-	int *map;
-	
-
+	cout << "The most common letters in " << filename << " are as follows:" << endl;
 	//having the map anonymized makes it so we do not have to worry about file management (score!)
-	map = (int*) mmap(NULL, FILESIZE,PROT_READ|PROT_WRITE,MAP_ANON| MAP_SHARED,-1,0);
-	
+	int *map = (int*) mmap(NULL, MMAP_FILESIZE,PROT_READ|PROT_WRITE,MAP_ANON| MAP_SHARED,-1,0);
 	OccurrenceHandler myObj;
 
 	//iterate through all ascii characters to fulfill tracking requirements
@@ -89,24 +130,21 @@ int main(int argc, char** argv)
 	}
 	// wait until all child processes terminate
 	while(wait(NULL) >= 0);
-	/*
-	//printing statement for all ascii chars
-	for(int i = 0; i < NUMINTS; i++)
-	{
-		cout <<((char)(i))<<":: "<< (map[i]) << endl;
-	}
-	*/
-	int* alphabet = (int*)calloc(0,26 * sizeof(int));
+	// create array of tuples for our data
+	tuple<char,int>* alphabet = (tuple<char,int>*)malloc(ALPHABET_LEN * sizeof(tuple<char,int>));
 	for(int i = 0; i < 26; i++)
 	{
-		alphabet[i] = map[LOWERCASE_OFFSET + i] + map[UPPERCASE_OFFSET + i];
+		// combine lower and upper case letter counts as sum
+		int sum = map[LOWERCASE_OFFSET + i] + map[UPPERCASE_OFFSET + i];
+		alphabet[i] = make_tuple((char)(UPPERCASE_OFFSET+i), sum );
 	}
+	//print of sorted characters
 
-	for(int i = 0; i < 26; i++)
-	{
-		cout<< ((char)(i+LOWERCASE_OFFSET)) << ": " << alphabet[i] << endl;
-	}
+	quickSort(alphabet, 0, ALPHABET_LEN -1);
+
+	outputData(alphabet,ALPHABET_LEN);
 	cout << "exiting program" << endl;
+	munmap(map,MMAP_FILESIZE);
 	return 0;
 
 }
